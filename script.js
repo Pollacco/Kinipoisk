7fc97b5accc023d93b81afdcee3d13fa95001270
Andrey <andrey.sviontek@gmail.com>
2019-06-20 12:33:56 +0500

    feat: rebuild function with fetch

--- script.js
+++ script.js
@@ -6,41 +6,26 @@ function apiSearch(event) {
   const searchText = document.querySelector('.form-control').value;
   const server = 'https://api.themoviedb.org/3/search/multi?api_key=4aa4a4d85fab6f23c378901370a0bc82&language=ru&query=' + searchText;
   movie.innerHTML = 'Загрузка..'; 
-  requestApi(server)
-    .then(function(result) {
-      const output = JSON.parse(result);
-      console.log(output);
-      let inner = '';
-      output.results.forEach((item) => {
-        let nameItem = item.name || item.title;
-        console.log(nameItem);
-        inner += `<div class="col-4">${nameItem}</div>`;
-      });
-      movie.innerHTML = inner; 
-      })
-    .catch()
-    ;
+  fetch(server)
+  .then(function(value) {
+    if (value.status !== 200) {
+      return Promise.reject(new Error('Ошибка'))
+    }
+
+    return value.json()
+  })
+  .then(function(output) {
+    let inner = '';
+    output.results.forEach((item) => {
+      let nameItem = item.name || item.title;
+      inner += `<div class="col-12 col-md-4 col-xl-3"> ${nameItem} </div>`;
+    });
+    movie.innerHTML = inner; 
+  })
+  .catch(function() {
+    movie.innerHTML = 'Произошла ошибка, повторите Ваш запрос'; 
+    console.log('error' + reason.status);
+  });
 }
 
 searchForm.addEventListener('submit', apiSearch);
-
-function requestApi(url) {
-  return new Promise(function(resolve, reject){
-    const request = new XMLHttpRequest();
-    request.open('GET', url);
-
-    request.addEventListener('load', function(){
-      if (request.status !== 200){
-        reject({status: request.status});
-        return;
-      }
-
-      resolve(request.response);
-
-    });
-    request.addEventListener('error', function(){
-      reject({status: request.status});
-    });
-    request.send();
-  });
-}
